da79a463ff64bdd17f63ca1f592a5284359a5fd0
zhujialei <jerru96868@gmail.com>
2019-02-11 16:19:56 +0800

    init commit

---
+++ 01_Python基础/test.py
@@ -0,0 +1,8 @@
+a = 1
+b = 10.5
+boo = True
+str = 'sefsegs'
+print(a + b)
+print(a + boo)
+print(b + boo)
+print(str + boo.__str__())
---
+++ 04_函数/list.py
@@ -0,0 +1,7 @@
+def bl_list(list):
+    for str in list:
+        print(str)
+
+
+list = ["ad","sdfe"]
+bl_list(list)
